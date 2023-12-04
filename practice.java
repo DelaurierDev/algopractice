6b01d7668e0f7e4dcbb0fa3c36b2f28a44d6ab1c
DelaurierDev <delaurier4@gmail.com>
2023-12-04 13:11:03 -0700

    solved question in java

--- practice.java
+++ practice.java
@@ -15,7 +15,33 @@ The final sorted array should not be returned by the function, but instead be st
 */
 
     public void merge(int[] nums1, int m, int[] nums2, int n){
+        // My solution
+        // Initialize a variable that keeps track of where we are in the array
+        int p = m + n - 1;
+        // Initialize 2 pointers that start at the end of where the elements begin in each array
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        //then we check which values are bigger and inster them into the array 
+        while(p1>=0 && p2>=0){
+            if(nums1[p1] > nums2[p2]){
+                nums1[p] = nums1[p1];
+                p--;
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p--;
+                p2--;
+            }
+        }
         
+        //implement this while loop for edge cases where nums1 is empty
+        while(p2>=0){
+            nums1[p] = nums2[p2];
+            p--;
+            p2--;
+        }
+
     }
 
 }
