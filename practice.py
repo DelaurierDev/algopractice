9d1a348aadb5eac88268d23b39742e392f7be060
DelaurierDev <delaurier4@gmail.com>
2023-12-04 13:17:50 -0700

    convert answer into python

--- practice.py
+++ practice.py
@@ -6,6 +6,21 @@
 
 #The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- var merge = function(nums1, m, nums2, n) {
-    
-};
+def merge(self, nums1, m, nums2, n):
+    p = m + n - 1
+    p1 = m - 1
+    p2 = n - 1
+
+    while(p1 >= 0 and p2 >= 0):
+        if(nums1[p1] > nums2[p2]):
+            nums1[p] = nums1[p1]
+            p -= 1
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p -= 1
+            p2 -= 1
+    while(p2>=0):
+        nums1[p] = nums2[p2]
+        p -= 1
+        p2 -= 1
