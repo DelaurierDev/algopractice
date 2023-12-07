7773a64e9f759cf040642f4115f090142997f540
DelaurierDev <delaurier4@gmail.com>
2023-12-06 18:56:35 -0700

    convert to python

--- practice.py
+++ practice.py
@@ -37,3 +37,26 @@ def removeElement(self, nums, val):
         nums.remove(val)
     return len(nums)
     
+
+#Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+#Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+#Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+#Return k.
+
+def removeDuplicates(self, nums):
+    k = 1
+    currVal = nums[0]
+    p1 = 1
+    p2 = 1
+    while(p2 < len(nums)):
+        if(nums[p2] == currVal):
+            p2 += 1
+        elif(nums[p2] != currVal):
+            currVal = nums[p2]
+            nums[p1] = currVal
+            p2 += 1
+            p1 += 1
+            k += 1
+    return k
