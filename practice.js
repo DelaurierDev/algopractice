4717c3daf21fd0742771570dffc27a6b857dbd15
DelaurierDev <delaurier4@gmail.com>
2023-12-07 19:17:48 -0700

    Solve majority element in javaScript

--- practice.js
+++ practice.js
@@ -89,4 +89,32 @@ var removeDuplicates = function(nums) {
         }
     }
     return k
+};  
+
+/*Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.*/
+
+var majorityElement = function(nums) {
+    const map = new Map()
+    for(let i = 0; i < nums.length; i++){
+        let key = nums[i]
+        if(map.get(key) >= 1){
+            val =map.get(key) 
+            val++
+            map.set(key, val)
+        }
+        else{
+            map.set(key, 1)
+        }
+    }
+
+    for (let [key, value] of  map.entries()) {
+        if(value >= nums.length / 2){
+            return key
+        }
+    }
+
 };
+
+majorityElement([2,2,1,1,1,2,2])
