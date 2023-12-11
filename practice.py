6f2b70e2081a2e5d65a97dad3ae69dfdc3b24eef
DelaurierDev <delaurier4@gmail.com>
2023-12-11 14:50:53 -0700

    solve is anagram in python

--- practice.py
+++ practice.py
@@ -66,3 +66,31 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 '''
+
+def isAnagram( s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+
+    dict = {}
+    for char in s:
+        if char in dict:
+            dict[char] = dict[char] + 1
+        else:
+            dict[char] = 1
+
+    for char in t:
+        if char in dict:
+            dict[char] = dict[char] - 1
+        else:
+            dict[char] = 1
+    for value in dict.values():
+        if value != 0:
+            return False
+
+    return True
+
+
+print(isAnagram("hello", "yo"))
