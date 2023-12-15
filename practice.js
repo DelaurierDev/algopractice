b93a279101d8d401b9cf7d46e51d23f9e2a9e29c
DelaurierDev <delaurier4@gmail.com>
2023-12-14 18:19:26 -0700

    solve needle in haystack in javascript

--- practice.js
+++ practice.js
@@ -258,3 +258,8 @@ var twoSum = function(numbers, target) {
 };
 
 console.log(twoSum([5,2575], 100))
+
+/*Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
