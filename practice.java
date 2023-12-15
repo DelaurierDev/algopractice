5c7baf25ac7c9684ebca078f2d7792116c2a6817
DelaurierDev <delaurier4@gmail.com>
2023-12-14 18:13:14 -0700

    solve in java

--- practice.java
+++ practice.java
@@ -179,4 +179,9 @@ public int[] twoSum(int[] numbers, int target) {
     }
     return new int[]{left + 1, right + 1};
 }
+
+/* Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.*/
+public int strStr(String haystack, String needle) {
+    return haystack.indexOf(needle);
+}
 }
