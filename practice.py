fa949f0fe1e7e63ad7430f7373afb71b4137e5a9
DelaurierDev <delaurier4@gmail.com>
2023-12-14 18:15:52 -0700

    solve in python

--- practice.py
+++ practice.py
@@ -151,3 +151,17 @@ def twoSum(self, numbers, target):
             left += 1
         else:
             right -= 1
+
+'''
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+'''
+
+def strStr(self, haystack, needle):
+    """
+    :type haystack: str
+    :type needle: str
+    :rtype: int
+    """
+    if needle in haystack:
+        return haystack.index(needle)
+    return -1
