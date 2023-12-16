ea88f957d0d7230d95e374458258be1578c888d9
DelaurierDev <delaurier4@gmail.com>
2023-12-15 20:31:13 -0700

    solvw is subsequence in js

--- practice.js
+++ practice.js
@@ -263,3 +263,23 @@ console.log(twoSum([5,2575], 100))
 var strStr = function(haystack, needle) {
     return haystack.indexOf(needle)
 };
+
+
+/* Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).*/
+
+var isSubsequence = function(s, t) {
+    let j = 0
+        for(let i = 0; i < t.length; i++){
+            if(s[j] == t[i]){
+                j++
+            }
+        }            
+        if(j == s.length  || s.length == 0){
+            return true
+                }
+    return false
+};
+
+console.log(isSubsequence("axc", "ahbgdc"))
