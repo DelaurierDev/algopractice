39d77628d4f0739c5847a0452718201317b1f44c
DelaurierDev <delaurier4@gmail.com>
2023-12-19 10:58:05 -0700

    convert answer to js

--- practice.js
+++ practice.js
@@ -303,3 +303,33 @@ var findDifference = function(nums1, nums2) {
 };
 
 console.log(findDifference([1,2,3,3],[1,1,2,2]))
+
+/* We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+ */
+
+var guessNumber = function(n) {
+    let low = 1
+    let high = n
+    let mid = (low + high) / 2
+    while (guess(mid) != 0){
+        if (guess(mid) == 1){
+        low = mid + 1}
+    else if (guess(mid) == -1){
+        high = mid - 1}
+    mid = (low + high) / 2
+    }
+    return mid
+
+}
