0386ffc6c24e10bade22a95c1afc8bc823d5401f
DelaurierDev <delaurier4@gmail.com>
2023-12-20 18:27:43 -0700

    solve kids with candies in python

--- practice.py
+++ practice.py
@@ -238,5 +238,31 @@ def guessNumber(n):
             high = mid - 1
         mid = (low + high) / 2
     return mid
+
+'''
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+'''
+
+def kidsWithCandies(self, candies, extraCandies):
+    """
+    :type candies: List[int]
+    :type extraCandies: int
+    :rtype: List[bool]
+    """
+    max = 0
+    for val in candies:
+        if val > max:
+            max = val
+    ans = []
+    for val in candies:
+        if val + extraCandies >= max:
+            ans.append(True)
+        else:
+            ans.append(False)
+    return ans
     
 
