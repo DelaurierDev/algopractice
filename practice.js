46d22579c9330a18232060d45e334451b3b9f359
DelaurierDev <delaurier4@gmail.com>
2023-12-20 18:22:00 -0700

    solve kids with candies in js.

--- practice.js
+++ practice.js
@@ -332,4 +332,27 @@ var guessNumber = function(n) {
     }
     return mid
 
+
+/* There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.*/
+
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = 0
+    let ans = []
+    for(let i = 0; i < candies.length; i++){
+        if (candies[i] > max){
+            max = candies[i]
+        }
+    }
+    for(let i = 0; i < candies.length; i++){
+        if(candies[i] + extraCandies >= max){
+            ans.push(true)
+       }
+       else{ ans.push(false)}
+    }
+    return ans
+};
 }
