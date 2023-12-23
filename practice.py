74b1205daf8a334c0368aa9dc2cf1bc19f8aabfd
DelaurierDev <delaurier4@gmail.com>
2023-12-22 22:10:59 -0700

    solve reverse vowels in python

--- practice.py
+++ practice.py
@@ -285,3 +285,31 @@ def maxProfit(self, prices):
         right += 1
     return max_profit
 
+'''
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+'''
+
+def reverseVowels( s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    s = list(s)
+    vowels = "aeiouAEIOU"
+    left = 0
+    right = len(s)-1
+    while left < right:
+        if s[left] in vowels and s[right] in vowels:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        elif s[left] not in vowels:
+            left += 1
+        elif s[right] not in vowels:
+            right -= 1
+    return ''.join(s)
+
+print(reverseVowels("hello"))
+
