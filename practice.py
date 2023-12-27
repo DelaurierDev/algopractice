dd7e0792ae537427abd21d432acf154750045fc0
DelaurierDev <delaurier4@gmail.com>
2023-12-26 18:40:00 -0700

    solve two sum in python

--- practice.py
+++ practice.py
@@ -332,4 +332,19 @@ def numberGame( nums):
     return arr
 numberGame([2,7,9,6,4,6])
 
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if (i != j and nums[i] + nums[j] == target):
+                return [i, j]
+        
+
 
