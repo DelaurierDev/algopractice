ae25f86dd44e0e0dd53abbe2feceba75e056ec5e
DelaurierDev <delaurier4@gmail.com>
2023-12-28 18:39:58 -0700

    analyze summaryRanges in python

--- practice.py
+++ practice.py
@@ -365,3 +365,24 @@ def isIsomorphic(self, s, t):
         return True
     return False
 
+'''
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+'''
+def summaryRanges(nums):
+    ranges = [] # [start, end] or [x, y]
+    for i, n in enumerate(nums):
+        if ranges and ranges[-1][1] == n-1:
+            ranges[-1][1] = n
+        else:
+            ranges.append([n, n])
+
+    return [f'{x}->{y}' if x != y else f'{x}' for x, y in ranges]
+        
+
+print(summaryRanges([0,1,2,4,5,7]))
